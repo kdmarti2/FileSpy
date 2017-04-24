438c93059f07845aa5bee6d416e8739b5447c5c2
Seth <sltroxle@ncsu.edu>
2017-04-24 02:51:12 -0400

    Shannon Entropy calculations work, checks are in place. Still need to act appropriately.

--- FileSpy/IOheuristics.h
+++ FileSpy/IOheuristics.h
@@ -14,11 +14,18 @@
 	IOTHold needs to be above 20 to allow for changes
 **/
 #define IOThold 20
+//Constant for converting log(x) to log2(x)
+#define M_LOG2E 1.44269504088896340736 //log2(e)
 
 void initIOheuristics();
 void IOhook(PCFLT_RELATED_OBJECTS FltObjects);
 void hhook(PCFLT_RELATED_OBJECTS FltObjects);
 void putProcess(Procmon* p);
 int shannonEntropy(PCFLT_RELATED_OBJECTS FltObjects, PUNICODE_STRING f);
+
+double approx_log(double x);
+double power(double x, int exp);
+double ln(double value);
+double log2(double value);
 
 #endif
